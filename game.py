66b73127e5c557c3b8de09642fc4ee7988dda3f0
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-02-20 23:51:39 +0100

    various tweaks

--- game.py
+++ game.py
@@ -5,68 +5,101 @@ import play
 from menu import GameState
 
 # Stałe wartości
-SCREEN_WIDTH = 900
-SCREEN_HEIGHT = 600
-FPS = 300  # Liczba klatek na sekundę
+SCREEN_WIDTH = 900  # Szerokość ekranu
+SCREEN_HEIGHT = 600  # Wysokość ekranu
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Utworzenie ekranu o określonych wymiarach
+INITIAL_STATE = GameState.MAIN_MENU  # Początkowy stan gry
 ESC_BUTTON = pygame.K_ESCAPE  # Przycisk ESC
 
 
-def handle_events(game_state):
-    """
-    Obsługuje zdarzenia pygame, takie jak naciśnięcie klawisza lub zamknięcie okna.
-    Zwraca nowy stan gry na podstawie zdarzeń.
-    """
+class Game:
 
-    event_actions = {
-        pygame.QUIT: GameState.QUIT,  # Zamknięcie okna jeśli GameState == QUIT
-        pygame.KEYDOWN: {  # Przycisk Escape
-            ESC_BUTTON: {  # Przycisk Escape
-                GameState.RUNNING: GameState.PAUSE,  # Pauza jeśli gra jest w trakcie
-                GameState.PAUSE: GameState.RUNNING,  # Wznów grę jeśli gra jest w pauzie
-                GameState.MAIN_MENU: GameState.QUIT,  # Wyjdź z gry jeśli jesteś w menu głównym
+    def __init__(self):
+        """
+        Inicjalizuje grę, ustawiając stan gry na MENU GŁÓWNE, tworząc ekran o określonych wymiarach.
+        """
+        self.state = INITIAL_STATE  # Początkowy stan gry
+        self.screen = SCREEN  # Utworzenie ekranu o określonych wymiarach
+
+    def get_events(self):
+        """
+        Pobiera zdarzenia z kolejki pygame.
+        """
+        return pygame.event.get()
+
+    def handle_events(self):
+        """
+        Obsługuje zdarzenia pygame, takie jak naciśnięcie klawisza lub zamknięcie okna.
+        Zwraca nowy stan gry na podstawie zdarzeń.
+        """
+        QUIT_EVENT = pygame.QUIT
+        KEYDOWN_EVENT = pygame.KEYDOWN
+
+        # Mapowanie zdarzeń na akcje
+        event_actions = {
+            QUIT_EVENT: GameState.QUIT,  # Zamknięcie okna powoduje zakończenie gry
+            KEYDOWN_EVENT: {  # Naciśnięcie klawisza
+                ESC_BUTTON: {  # Naciśnięcie klawisza Escape
+                    GameState.RUNNING: GameState.PAUSE,  # Pauza, jeśli gra jest w trakcie
+                    GameState.PAUSE: GameState.RUNNING,  # Wznów grę, jeśli gra jest w pauzie
+                    GameState.MAIN_MENU: GameState.QUIT,  # Wyjdź z gry, jeśli jesteś w menu głównym
+                },
             },
-        },
-    }
-    for event in pygame.event.get():  # Pobranie wszystkich zdarzeń z kolejki
-        if event.type in event_actions:  # Sprawdzenie czy zdarzenie jest obsługiwane
-            action = event_actions[event.type]  # Pobranie akcji dla danego zdarzenia
-            if isinstance(action, dict):  # Sprawdzenie czy akcja jest słownikiem
-                if event.type == pygame.KEYDOWN and event.key in action:  # Sprawdzenie czy klawisz jest obsługiwany
-                    return action[event.key].get(
-                        game_state,
-                        game_state,
-                    )  # Zwrócenie nowego stanu gry
-                return action.get(
-                    game_state,
-                    game_state,
-                )  # Zwrócenie nowego stanu gry
-            else:
-                return action
-    return game_state
+        }
+
+        events = self.get_events()  # Pobranie zdarzeń z kolejki pygame
+        # Przetwarzanie zdarzeń w kolejce
+        for event in events:
+            if event.type in event_actions:  # Sprawdzenie, czy zdarzenie jest obsługiwane
+                action = event_actions[event.type]  # Pobranie akcji dla danego zdarzenia
+                if isinstance(action, dict):  # Sprawdzenie, czy akcja jest słownikiem
+                    if (
+                        event.type == self.KEYDOWN_EVENT and event.key in action
+                    ):  # Sprawdzenie, czy klawisz jest obsługiwany
+                        # Zwrócenie nowego stanu gry lub aktualnego, jeśli nie ma zdefiniowanej akcji dla aktualnego stanu
+                        return action[event.key].get(self.state, self.state)
+                else:
+                    return action
+        return self.state  # Zwrócenie aktualnego stanu, jeśli nie ma zdarzeń do obsługi
+
+    def run(self):
+        """
+        Uruchamia główną pętlę gry, która trwa, dopóki stan gry nie jest równy QUIT.
+        W każdej iteracji pętli, zdarzenia są obsługiwane, a stan gry jest aktualizowany.
+        """
+        while self.state != GameState.QUIT:
+            self.state = self.handle_events()  # Obsługa zdarzeń i aktualizacja stanu gry
+            if self.state == GameState.MAIN_MENU:
+                self.main_menu()  # Wyświetlanie menu głównego
+            elif self.state == GameState.RUNNING:
+                self.game_running()  # Uruchomienie gry
+            elif self.state == GameState.PAUSE:
+                self.pause_menu()  # Wyświetlanie menu pauzy
+
+    def main_menu(self):
+        """
+        Wywołuje funkcję wyświetlającą menu główne gry.
+        """
+        self.state = menu.main_menu(self.screen, self.state)
+
+    def game_running(self):
+        """
+        Wywołuje funkcję uruchamiającą grę.
+        """
+        play.game_running(self.screen)
+
+    def pause_menu(self):
+        """
+        Wywołuje funkcję wyświetlającą menu pauzy gry.
+        """
+        menu.pause_menu(self.screen, self.state)
 
 
 def launch():
     """
-    Główna funkcja gry. Inicjalizuje pygame, tworzy okno gry,
-    a następnie uruchamia główną pętlę gry.
+    Inicjalizuje pygame, tworzy instancję klasy Game, uruchamia grę, a następnie kończy działanie pygame.
     """
-    pygame.init()  # Inicjalizacja pygame
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Utworzenie okna
-
-    game_state = GameState.MAIN_MENU  # Początkowy stan gry
-    clock = pygame.time.Clock()  # Obiekt do kontrolowania czasu
-
-    # Główna pętla gry
-    while game_state != GameState.QUIT:
-        clock.tick(FPS)  # Ograniczenie liczby klatek na sekundę
-        game_state = handle_events(game_state)  # Obsługa zdarzeń
-
-        # Obsługa różnych stanów gry
-        if game_state == GameState.MAIN_MENU:  # Obsługa menu głównego
-            game_state = menu.main_menu(screen, game_state)
-        elif game_state == GameState.RUNNING:  # Obsługa stanu gry "running"
-            play.game_running(screen)
-        elif game_state == GameState.PAUSE:  # Obsługa menu pauzy
-            menu.pause_menu(screen, game_state)
-
-    pygame.quit()  # Zamknięcie pygame po zakończeniu gry
+    pygame.init()
+    game = Game()
+    game.run()
+    pygame.quit()
