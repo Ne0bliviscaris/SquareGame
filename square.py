43410f320713a778d586ab77099d17f782965a75
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-02-29 17:03:14 +0100

    Refactor collision handling in Square class
    Simplified the logic for handling ground collision in the Square class. Removed redundant conditions and adjusted the center difference threshold for determining the square's position relative to the tile. This change improves the readability and maintainability of the code.

--- square.py
+++ square.py
@@ -43,36 +43,28 @@ class Square:
         """Obsługuje kolizję kwadratu z danym kafelkiem."""
         is_above_and_falling = self.y < tile.y and self.velocity > 0
         is_below_and_rising = self.y > tile.y and self.velocity < 0
-        is_left_and_moving_right = self.x < tile.x and self.velocity_x > 0
-        is_right_and_moving_left = self.x > tile.x and self.velocity_x < 0
-        is_right_and_moving_left_fast = (
-            self.x > tile.x and self.velocity_x < 0 and abs(self.x - (tile.x + tile.size)) < self.velocity_x
-        )
+        is_moving_horizontally = self.velocity_x != 0
+        is_moving_left = self.x > tile.x and self.velocity_x < 0
 
         if is_above_and_falling:
             self.velocity = 0
             self.y = tile.y - self.size
-
         elif is_below_and_rising:
             # Oblicz różnicę między środkiem kwadratu a środkiem kafelka
             center_diff = (self.x + self.size / 2) - (tile.x + tile.size / 2)
 
-            # Jeśli środek kwadratu jest przesunięty o więcej niż 40% w lewo od środka kafelka
-            if center_diff < -0.4 * tile.size:
+            # Jeśli środek kwadratu jest przesunięty o więcej niż 20% w lewo od środka kafelka
+            if center_diff < -0.2 * tile.size:
                 self.x = (self.x // tile.size) * tile.size
-            # Jeśli środek kwadratu jest przesunięty o więcej niż 40% w prawo od środka kafelka
-            elif center_diff > 0.4 * tile.size:
+            # Jeśli środek kwadratu jest przesunięty o więcej niż 20% w prawo od środka kafelka
+            elif center_diff > 0.2 * tile.size:
                 self.x = (self.x // tile.size + 1) * tile.size
             else:
                 self.y = tile.y + tile.size
                 self.velocity = 0
-
-        elif is_left_and_moving_right:
+        elif is_moving_horizontally:
             self.velocity_x = 0
-            self.x = tile.x - self.size
-        elif is_right_and_moving_left:
-            self.velocity_x = 0
-            self.x = tile.x + tile.size
-        elif is_right_and_moving_left_fast:
-            self.velocity_x = 0
-            self.x = tile.x + tile.size
+            if is_moving_left:
+                self.x = tile.x + self.size
+            else:
+                self.x = tile.x - self.size
