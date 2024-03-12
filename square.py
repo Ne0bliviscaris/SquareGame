a5457f35269772c9ff82c7760923b626398b8283
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-03-12 16:48:52 +0100

    separated ai

--- square.py
+++ square.py
@@ -61,6 +61,10 @@ class Square:
             ),
         )
 
+    def collides_with(self, other):
+        """Sprawdza, czy kwadrat koliduje z innym obiektem."""
+        return self.rect.colliderect(other.rect)
+
 
 class Player(Square):
     """Klasa reprezentująca kwadrat gracza."""
@@ -113,45 +117,4 @@ class Player(Square):
                 square,
             ),
             int(9 * zoom_level),  # Szerokość obramowania
-        )
-
-
-class Ai(Square):
-    """Klasa reprezentująca kwadrat przeciwnika."""
-
-    def __init__(self, x, y, size, mode):
-        """Inicjalizuje kwadrat na podanej pozycji i o podanym rozmiarze."""
-        super().__init__(x, y, size)
-        self.mode = mode  # Dodajemy tryb
-        self.color = CATCH_COLOR if mode == "catch" else FLEE_COLOR  # Ustalamy kolor na podstawie trybu
-
-    def change_mode(self):
-        """Zmienia tryb AI."""
-        super().change_mode()
-        self.color = CATCH_COLOR if self.mode == "catch" else FLEE_COLOR
-
-    def update(self, mode):
-        """Aktualizuje pozycję kwadratu, dodając do niej prędkość."""
-        super().update()
-        if self.mode == "catch":
-            ...
-        else:
-            ...
-        # Dodaj tutaj logikę AI, która steruje ruchem przeciwnika
-
-    def draw(self, screen, camera_offset_x=0, camera_offset_y=0, zoom_level=1):
-        """Rysuje kwadrat na ekranie."""
-        # Ustalenie pozcji i wymiarów
-        left = int(self.x * zoom_level) + camera_offset_x + 1
-        top = int(self.y * zoom_level) + camera_offset_y + 1
-        square = round(self.size * zoom_level)
-        draw.rect(
-            screen,
-            self.color,  # Używamy koloru zależnego od trybu
-            Rect(
-                left,
-                top,
-                square,
-                square,
-            ),
         )
