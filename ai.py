d39de0023469caf4a08b127b9c1981859b42361a
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-03-12 16:48:41 +0100

    separated ai

---
+++ ai.py
@@ -0,0 +1,44 @@
+from pygame import Rect, draw
+
+from square import CATCH_COLOR, FLEE_COLOR, PLAYER_COLOR, Square
+
+
+class Ai(Square):
+    """Klasa reprezentująca kwadrat przeciwnika."""
+
+    def __init__(self, x, y, size, mode="flee"):
+        """Inicjalizuje kwadrat na podanej pozycji i o podanym rozmiarze."""
+        super().__init__(x, y, size)
+        self.mode = mode  # Dodajemy tryb
+        self.color = CATCH_COLOR if mode == "catch" else FLEE_COLOR  # Ustalamy kolor na podstawie trybu
+
+    def change_mode(self):
+        """Zmienia tryb AI."""
+        super().change_mode()
+        self.color = CATCH_COLOR if self.mode == "catch" else FLEE_COLOR
+
+    def update(self):
+        """Aktualizuje pozycję kwadratu, dodając do niej prędkość."""
+        super().update()
+        if self.mode == "catch":
+            ...
+        else:
+            ...
+        # Dodaj tutaj logikę AI, która steruje ruchem przeciwnika
+
+    def draw(self, screen, camera_offset_x=0, camera_offset_y=0, zoom_level=1):
+        """Rysuje kwadrat na ekranie."""
+        # Ustalenie pozcji i wymiarów
+        left = int(self.x * zoom_level) + camera_offset_x + 1
+        top = int(self.y * zoom_level) + camera_offset_y + 1
+        square = round(self.size * zoom_level)
+        draw.rect(
+            screen,
+            self.color,  # Używamy koloru zależnego od trybu
+            Rect(
+                left,
+                top,
+                square,
+                square,
+            ),
+        )
