f72da5c5a0f5cdb0b26a7f9ada416e6f0d7904a2
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-03-12 15:52:51 +0100

    introduced square types and identification

--- square.py
+++ square.py
@@ -2,13 +2,15 @@ from math import ceil, floor
 
 from pygame import Rect, draw
 
-SQUARE_COLOR = (180, 0, 0)
+PLAYER_COLOR = (0, 180, 0)
+CATCH_COLOR = (180, 0, 0)
+FLEE_COLOR = (0, 0, 180)
 
 
 class Square:
     """Klasa reprezentująca kwadrat na ekranie."""
 
-    def __init__(self, x, y, size):
+    def __init__(self, x, y, size, mode="flee"):
         """Inicjalizuje kwadrat na podanej pozycji i o podanym rozmiarze."""
         self.x = x
         self.y = y
@@ -16,11 +18,22 @@ class Square:
         self.velocity_y = 0
         self.velocity_x = 0  # Dodajemy prędkość w osi x
         self.gravity = 0.12
+        self.mode = mode
+        self.color = PLAYER_COLOR if mode == "catch" else FLEE_COLOR
 
     def move(self, dx, dy):
         """Przesuwa kwadrat o daną ilość pikseli."""
         self.x += dx
         self.y += ceil(dy)
+
+    def change_mode(self):
+        """Zmienia tryb kwadratu."""
+        if self.mode == "catch":
+            self.mode = "flee"
+            self.color = FLEE_COLOR
+        else:
+            self.mode = "catch"
+            self.color = CATCH_COLOR
 
     @property
     def rect(self):
@@ -39,7 +52,7 @@ class Square:
         square = round(self.size * zoom_level)
         draw.rect(
             screen,
-            SQUARE_COLOR,
+            PLAYER_COLOR,
             Rect(
                 left,
                 top,
@@ -51,6 +64,10 @@ class Square:
 
 class Player(Square):
     """Klasa reprezentująca kwadrat gracza."""
+
+    def change_mode(self):
+        """Zmienia tryb gracza."""
+        super().change_mode()
 
     def move_left(self, speed):
         """Przesuwa kwadrat w lewo."""
@@ -65,12 +82,76 @@ class Player(Square):
         if self.velocity_y == 0:
             self.velocity_y = -8
 
+    def draw(self, screen, camera_offset_x=0, camera_offset_y=0, zoom_level=1):
+        """Rysuje kwadrat na ekranie."""
+        # Ustalenie pozcji i wymiarów
+        left = int(self.x * zoom_level) + camera_offset_x + 1
+        top = int(self.y * zoom_level) + camera_offset_y + 1
+        square = round(self.size * zoom_level)
+        inner_color = CATCH_COLOR if self.mode == "catch" else FLEE_COLOR  # Używamy koloru zależnego od trybu
+
+        # Rysowanie kwadratu
+        draw.rect(
+            screen,
+            inner_color,
+            Rect(
+                left,
+                top,
+                square,
+                square,
+            ),
+        )
+
+        # Rysowanie obramowania
+        draw.rect(
+            screen,
+            PLAYER_COLOR,
+            Rect(
+                left,
+                top,
+                square,
+                square,
+            ),
+            int(9 * zoom_level),  # Szerokość obramowania
+        )
+
 
 class AI(Square):
     """Klasa reprezentująca kwadrat przeciwnika."""
 
-    def update(self):
+    def __init__(self, x, y, size, mode):
+        """Inicjalizuje kwadrat na podanej pozycji i o podanym rozmiarze."""
+        super().__init__(x, y, size)
+        self.mode = mode  # Dodajemy tryb
+        self.color = CATCH_COLOR if mode == "catch" else FLEE_COLOR  # Ustalamy kolor na podstawie trybu
+
+    def change_mode(self):
+        """Zmienia tryb AI."""
+        super().change_mode()
+        self.color = CATCH_COLOR if self.mode == "catch" else FLEE_COLOR
+
+    def update(self, mode):
         """Aktualizuje pozycję kwadratu, dodając do niej prędkość."""
         super().update()
-
+        if self.mode == "catch":
+            ...
+        else:
+            ...
         # Dodaj tutaj logikę AI, która steruje ruchem przeciwnika
+
+    def draw(self, screen, camera_offset_x=0, camera_offset_y=0, zoom_level=1):
+        """Rysuje kwadrat na ekranie."""
+        # Ustalenie pozcji i wymiarów
+        left = int(self.x * zoom_level) + camera_offset_x + 1
+        top = int(self.y * zoom_level) + camera_offset_y + 1
+        square = round(self.size * zoom_level)
+        draw.rect(
+            screen,
+            self.color,  # Używamy koloru zależnego od trybu
+            Rect(
+                left,
+                top,
+                square,
+                square,
+            ),
+        )
