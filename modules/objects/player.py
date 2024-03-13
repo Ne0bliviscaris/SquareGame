2f90ee5e68c5ddbd04ec1ad1d5504626a352c92a
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-03-13 17:07:40 +0100

    separated player from square

---
+++ modules/objects/player.py
@@ -0,0 +1,61 @@
+from pygame import Rect, draw
+
+from square import CATCH_COLOR, FLEE_COLOR, OBSERVER_COLOR, PLAYER_COLOR, Square
+
+
+class Player(Square):
+    """Klasa reprezentująca kwadrat gracza."""
+
+    def change_mode(self):
+        """Zmienia tryb gracza."""
+        super().change_mode()
+
+    def draw(self, screen, camera_offset_x=0, camera_offset_y=0, zoom_level=1):
+        """Rysuje kwadrat na ekranie."""
+        # Ustalenie pozcji i wymiarów
+        left = int(self.x * zoom_level) + camera_offset_x + 1
+        top = int(self.y * zoom_level) + camera_offset_y + 1
+        square = round(self.size * zoom_level)
+
+        if self.mode == "catch":
+            inner_color = CATCH_COLOR
+        elif self.mode == "observer":
+            inner_color = OBSERVER_COLOR
+        else:
+            inner_color = FLEE_COLOR
+        # Rysowanie kwadratu
+        draw.rect(
+            screen,
+            inner_color,
+            Rect(
+                left,
+                top,
+                square,
+                square,
+            ),
+        )
+        if self.collide:
+            draw.rect(
+                screen,
+                (255, 255, 255),  # Biały kolor
+                Rect(
+                    left,
+                    top,
+                    square,
+                    square,
+                ),
+                50,  # Szerokość ramki
+            )
+
+        # Rysowanie obramowania
+        draw.rect(
+            screen,
+            PLAYER_COLOR,
+            Rect(
+                left,
+                top,
+                square,
+                square,
+            ),
+            int(10 * zoom_level),  # Szerokość obramowania
+        )
--- play.py
+++ play.py
@@ -6,11 +6,11 @@ from ai import Ai
 from modules.ai.vectors import VectorCalculator
 from modules.behavior.camera import Camera
 from modules.behavior.collisions import Collisions
+from modules.objects.player import Player
 from modules.objects.tiles import Ground
 from modules.state import GameState
 from modules.world.world import TILE_SIZE, WORLD_WIDTH
 from modules.world.world_builder import world_list
-from square import Player
 
 FPS_LIMIT = 250
 RUNNERS = 2
