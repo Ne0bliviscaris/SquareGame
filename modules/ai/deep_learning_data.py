a0202c9bc92291d84c7b67229ddeff99512763e5
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-03-18 19:49:31 +0100

    Predict working in AI class (not passing further yet)

---
+++ modules/ai/deep_learning_data.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from modules.ai.vectors import VectorCalculator
+
+
+class DeepLearningData:
+    def __init__(self, squares):
+        self.squares = squares
+        self.vector_calculator = VectorCalculator(self.squares)
+
+    def get_state(self):
+        positions_x = [square.x for square in self.squares]
+        positions_y = [square.y for square in self.squares]
+        velocity_x = [square.velocity_x for square in self.squares]
+        velocity_y = [square.velocity_y for square in self.squares]
+        speeds = [square.speed for square in self.squares]
+        collisions = [square.collide for square in self.squares]
+        # vectors = self.vector_calculator.calculate_vectors()
+
+        # Przekształć listy na numpy arrays
+        positions_x = np.array(positions_x)
+        positions_y = np.array(positions_y)
+        velocity_x = np.array(velocity_x)
+        velocity_y = np.array(velocity_y)
+        speeds = np.array(speeds)
+        collisions = np.array(collisions)
+        # vectors = np.array(vectors)
+
+        # Połącz wszystkie arrays w jeden tensor
+        # state = np.concatenate((positions_x, positions_y, velocity_x, velocity_y, speeds, collisions, vectors), axis=0)
+        state = np.concatenate((positions_x, positions_y, velocity_x, velocity_y, speeds, collisions), axis=0)
+
+        return state
