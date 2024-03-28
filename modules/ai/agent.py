390660ec3c58f2cd7b7f0fc2584e99e014930a1a
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-03-28 16:57:09 +0100

    switched deep learning to cuda cores

--- modules/ai/agent.py
+++ modules/ai/agent.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-from modules.ai.deep_learning_data import PARAMETERS_LENGTH, DeepLearningData
+from modules.ai.deep_learning_data import PARAMETERS_LENGTH
 from modules.settings import TOTAL_SQUARES
 
 
@@ -9,7 +9,8 @@ class DeepLearningAgent:
     """Agent AI, który wykorzystuje sieć neuronową do podejmowania decyzji."""
 
     def __init__(self):
-        self.network = self.create_network()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.network = self.create_network().to(self.device)
         self.optimizer = torch.optim.Adam(self.network.parameters())
         self.loss_function = nn.MSELoss()
         self.load_model()
@@ -19,7 +20,7 @@ class DeepLearningAgent:
         return nn.Sequential(nn.Linear(data_matrix_length, 256), nn.ReLU(), nn.Linear(256, 3), nn.Softmax(dim=-1))
 
     def predict(self, game_state):
-        state_tensor = torch.tensor(game_state, dtype=torch.float32)
+        state_tensor = torch.tensor(game_state, dtype=torch.float32).to(self.device)
         action_probabilities = self.network(state_tensor)
         action = torch.argmax(action_probabilities).item()
         return action
@@ -37,8 +38,8 @@ class DeepLearningAgent:
 
     def train(self, game_state, target):
         self.optimizer.zero_grad()  # Zeruj gradienty
-        state_tensor = torch.tensor(game_state, dtype=torch.float32)
-        target_tensor = torch.full((3,), target, dtype=torch.float32)
+        state_tensor = torch.tensor(game_state, dtype=torch.float32).to(self.device)
+        target_tensor = torch.full((3,), target, dtype=torch.float32).to(self.device)
         output = self.network(state_tensor)  # Przewidywania modelu
         loss = self.loss_function(output, target_tensor)  # Oblicz strate
         loss.backward()  # Oblicz gradienty
